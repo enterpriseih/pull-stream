76f5109c81212fd05538e33a56f875ecaf9a0e20
Zian-Andrew-GONG <ziangong0714@gmail.com>
2022-06-20 18:13:22 +0800

    rm -r cmake-build

--- cmake-build/third-party/googletest-release-1.11.0/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gongzian/src/pull-stream/third-party/googletest-release-1.11.0/googlemock
-# Build directory: /Users/gongzian/src/pull-stream/cmake-build/third-party/googletest-release-1.11.0/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
