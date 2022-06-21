5e457f7f2508d704eb979be2d55244b5ebe03d44
Zian-Andrew-GONG <ziangong0714@gmail.com>
2022-06-21 16:12:20 +0800

    add source state

--- cmake-build/third-party/googletest-release-1.11.0/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gongzian/src/pull-stream/third-party/googletest-release-1.11.0/googletest
-# Build directory: /Users/gongzian/src/pull-stream/cmake-build/third-party/googletest-release-1.11.0/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
